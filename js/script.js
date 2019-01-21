fd9ac61c2f0bc659a85d8336db5c73740a2a3afc
Autif Kamal <kautif@gmail.com>
2019-01-21 15:49:22 -0600

    updated projects interface

--- js/script.js
+++ js/script.js
@@ -1,43 +1,39 @@
-$('#bio-link').on('click', function(e) {
+$('.bio-link').on('click', function(e) {
 	$('.bio').show();
 	$('.resume').hide();
 	$('.projects').hide();
 	$('.contact').hide();
-	$('#bio-link').addClass('active');
-	$('#projects-link').removeClass('active');
-	$('#resume-link').removeClass('active');
-	$('#contact-link').removeClass('active');
+	$('.bio-link').addClass('active');
+	$('.projects-link').removeClass('active');
+	$('.resume-link').removeClass('active');
+	$('.contact-link').removeClass('active');
 });
 
-$('#projects-link').on('click', function(e) {
+$('.projects-link').on('click', function(e) {
 	$('.projects').show();
 	$('.bio').hide();
 	$('.resume').hide();
 	$('.contact').hide();
-	$('#projects-link').addClass('active');
-	$('#bio-link').removeClass('active');
-	$('#resume-link').removeClass('active');
-	$('#contact-link').removeClass('active');
+	$('.projects-link').addClass('active');
+	$('.bio-link').removeClass('active');
+	$('.resume-link').removeClass('active');
+	$('.contact-link').removeClass('active');
 });
 
-$('#resume-link').on('click', function(e) {
-	$('.resume').show();
-	$('.bio').hide();
-	$('.projects').hide();
-	$('.contact').hide();
-	$('#resume-link').addClass('active');
-	$('#projects-link').removeClass('active');
-	$('#bio-link').removeClass('active');
-	$('#contact-link').removeClass('active');
+$('.resume-link').on('click', function(e) {
+	$('.resume-link').addClass('active');
+	$('.projects-link').removeClass('active');
+	$('.bio-link').removeClass('active');
+	$('.contact-link').removeClass('active');
 });
 
-$('#contact-link').on('click', function(e) {
+$('.contact-link').on('click', function(e) {
 	$('.contact').show();
 	$('.bio').hide();
 	$('.projects').hide();
 	$('.resume').hide();
-	$('#contact-link').addClass('active');
-	$('#projects-link').removeClass('active');
-	$('#resume-link').removeClass('active');
-	$('#bio-link').removeClass('active');
+	$('.contact-link').addClass('active');
+	$('.projects-link').removeClass('active');
+	$('.resume-link').removeClass('active');
+	$('.bio-link').removeClass('active');
 });

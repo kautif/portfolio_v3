113707dd5f2717e37a188f3302713678a4998c8d
Autif Kamal <kautif@gmail.com>
2023-05-06 14:07:01 -0500

    added games section and updated corresponding styles

--- js/script.js
+++ js/script.js
@@ -2,19 +2,36 @@ $('.bio-link').on('click', function(e) {
 	$('.bio').show();
 	$('.resume').hide();
 	$('.projects').hide();
+	$('.games').hide();
 	$('.contact').hide();
 	$('.bio-link').addClass('active');
 	$('.projects-link').removeClass('active');
+	$('.games-link').removeClass('active');
 	$('.resume-link').removeClass('active');
 	$('.contact-link').removeClass('active');
 });
 
 $('.projects-link').on('click', function(e) {
 	$('.projects').show();
+	$('.games').hide();
 	$('.bio').hide();
 	$('.resume').hide();
 	$('.contact').hide();
 	$('.projects-link').addClass('active');
+	$('.games-link').removeClass('active');
+	$('.bio-link').removeClass('active');
+	$('.resume-link').removeClass('active');
+	$('.contact-link').removeClass('active');
+});
+
+$('.games-link').on('click', function(e) {
+	$('.games').show();	
+	$('.projects').hide();
+	$('.bio').hide();
+	$('.resume').hide();
+	$('.contact').hide();
+	$('.games-link').addClass('active');
+	$('.projects-link').removeClass('active');
 	$('.bio-link').removeClass('active');
 	$('.resume-link').removeClass('active');
 	$('.contact-link').removeClass('active');
@@ -23,6 +40,7 @@ $('.projects-link').on('click', function(e) {
 $('.resume-link').on('click', function(e) {
 	$('.resume-link').addClass('active');
 	$('.projects-link').removeClass('active');
+	$('.games-link').removeClass('active');
 	$('.bio-link').removeClass('active');
 	$('.contact-link').removeClass('active');
 });
@@ -31,9 +49,11 @@ $('.contact-link').on('click', function(e) {
 	$('.contact').show();
 	$('.bio').hide();
 	$('.projects').hide();
+	$('.games').hide();
 	$('.resume').hide();
 	$('.contact-link').addClass('active');
 	$('.projects-link').removeClass('active');
+	$('.games-link').removeClass('active');
 	$('.resume-link').removeClass('active');
 	$('.bio-link').removeClass('active');
 });
